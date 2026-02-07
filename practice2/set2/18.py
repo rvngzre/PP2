368d9807d83b114545c106854022fc115fa12df4
rvngzre <ergazyulyd@icloud.com>
2026-02-07 15:29:07 +0500

    complete

--- practice2/set2/18.py
+++ practice2/set2/18.py
@@ -8,7 +8,7 @@ first_pos = {}
 for i in range(n):
     s = arr[i]
     if s not in first_pos:
-        first_pos[s] = i + 1  # индекс с 1
+        first_pos[s] = i + 1
 
 for s in sorted(first_pos):
     print(s, first_pos[s])
